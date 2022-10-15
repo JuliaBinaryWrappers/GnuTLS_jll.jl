ca8ba05bfe912ac01ed31908edc42004d56cb19d
jlbuild <juliabuildbot@gmail.com>
2022-10-15 15:57:07 +0000

    GnuTLS_jll build 3.7.8+1

--- src/wrappers/armv6l-linux-gnueabihf.jl
+++ src/wrappers/armv6l-linux-gnueabihf.jl
@@ -4,10 +4,11 @@ export libgnutls
 using Zlib_jll
 using GMP_jll
 using Nettle_jll
+using P11Kit_jll
 JLLWrappers.@generate_wrapper_header("GnuTLS")
 JLLWrappers.@declare_library_product(libgnutls, "libgnutls.so.30")
 function __init__()
-    JLLWrappers.@generate_init_header(Zlib_jll, GMP_jll, Nettle_jll)
+    JLLWrappers.@generate_init_header(Zlib_jll, GMP_jll, Nettle_jll, P11Kit_jll)
     JLLWrappers.@init_library_product(
         libgnutls,
         "lib/libgnutls.so",
